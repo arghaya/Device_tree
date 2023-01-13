7275d1a8fb52d752bd17aa52cdd6743b651b0838
pc1598 <piyushchouhan1598@gmail.com>
2023-01-13 09:12:54 +0000

    Revert "raphael: parts: Introduce haptic feedback level adjustment"
    This reverts commit 92328d6ae6b9187d88f65ab0bd13a9b979c9f7e6.

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -23,7 +23,6 @@ import android.content.Intent;
 import android.util.Log;
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
-import org.lineageos.settings.haptic.HapticUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.utils.FileUtils;
 import android.content.SharedPreferences;
@@ -47,7 +46,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
             Log.d(TAG, "Dirac is not present in system");
         }
         DozeUtils.checkDozeService(context);
-        HapticUtils.restoreLevel(context);
         PopupCameraUtils.startService(context);
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
